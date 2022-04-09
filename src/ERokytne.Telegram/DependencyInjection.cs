71ce4b9f312d86727e6ff6e5ee6c10ad937777bc
Dimitry Sukalo <dimitrysukalo@gmail.com>
2022-04-09 22:09:01 +0300

    Add logs

--- src/ERokytne.Telegram/DependencyInjection.cs
+++ src/ERokytne.Telegram/DependencyInjection.cs
@@ -10,8 +10,14 @@ public static class DependencyInjection
 {
     public static void AddTelegramBot(this IServiceCollection services, IConfiguration configuration)
     {
-        var telegramBot = new TelegramBotClient(configuration.GetValue<string>("Services:TelegramBot:Token"));
-        telegramBot.SetWebhookAsync(configuration.GetValue<string>("Services:TelegramBot:Url"));
+        var token = configuration.GetValue<string>("Services:TelegramBot:Token");
+        var url = configuration.GetValue<string>("Services:TelegramBot:Url");
+
+        Console.WriteLine($"Token: {token}. Url: {url}");
+        
+        var telegramBot = new TelegramBotClient(token);
+        telegramBot.SetWebhookAsync(url);
+        Console.WriteLine("Webhook was setted");
         
         services.AddSingleton<ITelegramBotClient, TelegramBotClient>(_ => telegramBot);
         services.AddScoped<ITelegramBotCommandHelper, TelegramBotCommandHelper>();
