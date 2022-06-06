115917d4c9e7f0a7a8e9f4a3206fde317c67b01f
Dimitry Sukalo <dimitrysukalo@gmail.com>
2022-06-06 11:06:26 +0300

    Refactoring

--- src/ERokytne.Application/Telegram/Commands/NotFoundCommand.cs
+++ src/ERokytne.Application/Telegram/Commands/NotFoundCommand.cs
@@ -7,12 +7,12 @@ using Telegram.Bot;
 
 namespace ERokytne.Application.Telegram.Commands;
 
-public class NotFoundCommand : IRequest<int>
+public class NotFoundCommand : IRequest
 {
     public long ChatId { get; set; }
 }
 
-public class NotFoundCommandHandler : IRequestHandler<NotFoundCommand, int>
+public class NotFoundCommandHandler : IRequestHandler<NotFoundCommand>
 {
     private readonly ITelegramBotClient _bot;
     private readonly ApplicationDbContext _dbContext;
@@ -23,7 +23,7 @@ public class NotFoundCommandHandler : IRequestHandler<NotFoundCommand, int>
         _dbContext = dbContext;
     }
 
-    public async Task<int> Handle(NotFoundCommand request, CancellationToken cancellationToken)
+    public async Task<Unit> Handle(NotFoundCommand request, CancellationToken cancellationToken)
     {
         _ = await _dbContext.TelegramUsers
                 .FirstOrDefaultAsync(e => e.ChatId == request.ChatId.ToString() && !e.IsRemoved, 
@@ -34,6 +34,6 @@ public class NotFoundCommandHandler : IRequestHandler<NotFoundCommand, int>
             Localizer.Messages.Get(BotConstants.Messages.NotFound.CommandIsNotFoundMessage),
             cancellationToken: cancellationToken);
         
-        return 999;
+        return Unit.Value;
     }
 }
